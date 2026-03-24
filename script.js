cb40ad1a1eaa757a2c7bbe2b3e384cffa704f3d4
JonathanJanke <261195173+JonathanJanke@users.noreply.github.com>
2026-03-24 14:49:00 +0100

    got template-functions outsourced to templates.js

--- script.js
+++ script.js
@@ -3,32 +3,6 @@ function init () {
     renderCategories();
 };
 
-function getHTMLForMenu (category, i) {
-    return `
-             <div class="category">
-                <img src="" alt="">
-                <h2>${category}</h2>
-                <div id="dishes-${category}"
-                </div>
-             </div>`
-}
-function getHTMLForDishes (dish) {
-    return `
-             <div class="dish">
-                <img src="#">
-                <div class="dish-info">
-                    <div class="dish-info-head">
-                        <h2>${dish.name}</h2>
-                        <span>${dish.price}</span>
-                    </div>
-                    <div class="dish-info-main">
-                        <span>${dish.description}</span>
-                    </div>
-                    <div class="dish-info-footer">
-                        <button>Add to basket</button>
-                    </div>
-             </div>`
-}
 
 function renderCategories () {
     const menu = document.getElementById("menu");
